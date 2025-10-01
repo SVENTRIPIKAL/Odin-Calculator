21c0275c96e4bc58baaa1d3ba9d6fa96cb9d184b
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-10-01 22:06:21 +0000

    Organize lines

--- javascript/main.js
+++ javascript/main.js
@@ -6,19 +6,13 @@ let displayTotal = ""
 // CALCULATION FUNCTIONS
 
 // adds two numbers
-function add(a, b) {
-    return a + b
-}
+function add(a, b) { return a + b }
 
 // subtracts two numbers
-function subtract(a, b) {
-    return a - b
-}
+function subtract(a, b) { return a - b }
 
 // multiplies two numbers
-function multiply(a, b) {
-    return a * b
-}
+function multiply(a, b) { return a * b }
 
 // divides two numbers, if possible
 function divide(a, b) {
